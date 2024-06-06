b94a6e016021b7de4b9e48fbf1a1fa2669a3f3dd
berk_guler <berk_guler@beko.com>
2024-06-06 21:09:22 +0300

    experiment for qe added

--- src/Experiment.py
+++ src/Experiment.py
@@ -6,6 +6,8 @@ pt.init()
 dataset = pt.get_dataset('irds:dpr-w100/natural-questions/dev')
 index_ref = pt.IndexRef.of('./indices/dpr-w100')
 pipeline = pt.BatchRetrieve(index_ref, wmodel='BM25')
+bm25_qe = pt.BatchRetrieve(index_ref, wmodel="BM25", controls={"qe" : "on"})
+
 # (optionally other pipeline components)
 
 print("in here1")
@@ -17,4 +19,18 @@ result = pt.Experiment(
 )
 print("Result:")
 print(result)
+print("in here3")
+
+print("------------------------")
+print("------------------------")
+
+print("in here4")
+result2 = pt.Experiment(
+    [bm25_qe],
+    dataset.get_topics('text'),
+    dataset.get_qrels(),
+    [MAP, nDCG@20]
+)
+print("Result bm25_qe:")
+print(result2)
 print("in here3")
